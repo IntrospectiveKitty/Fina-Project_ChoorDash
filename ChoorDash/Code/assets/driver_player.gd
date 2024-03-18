14b835bc48a32a009b371d5225d0ae8aa72111d1
RC07227u11 <RC07227u11@ad.pcc.edu>
2024-03-18 16:01:04 -0700

    added movement

--- ChoorDash/Code/assets/driver_player.gd
+++ ChoorDash/Code/assets/driver_player.gd
@@ -2,27 +2,34 @@ extends CharacterBody2D
 
 var speed = 200
 
-func _physics_process(delta):
+func _process(delta):
 	#give player direction, vector(x,y)
 	var velocity = Vector2(0,-1)
 	if Input.is_action_pressed("move_right"):
 		#move the horizontal dirc: velocity (x) 
-		velocity.x = 1
+		rotation_degrees = 270
+		velocity.x = speed
 		velocity.y = 0
-		
-	
+		position += velocity * delta
+
 	if Input.is_action_pressed("move_left"):
 		velocity.x = -1
 		velocity.y = 0
+		position += velocity * delta
 		
+
 	if Input.is_action_pressed("move_up"):
 		velocity.y = -1 
 		velocity.x = 0
+		position += velocity * delta
 		
+
 	if Input.is_action_pressed("move_down"):
 		velocity.y = 1
 		velocity.x = 0
-		
-	velocity = velocity.normalized() 
-	
+		position += velocity * delta
+
+
+	#velocity = velocity.normalized()
+
 	#move_and_slide(velocity * speed)
